ec00f5c6f87d24fea2a48f1b7899c7fda7776192
lateg <lategano.antonio@gmail.com>
2015-12-13 10:27:20 +0100

    First release.

--- client.c
+++ client.c
@@ -4,29 +4,45 @@
 
 #if defined WIN32
 #include <winsock.h>
-#else #define closesocket close
-
+#else
+#define closesocket close
 #include <sys/socket.h>
-#include <arpa/inet h>
 #include <arpa/inet.h>
 #include <unistd.h>
-
 #endif
 
 #include <stdio.h>
 #include <string.h> /* for memset() */
 
-#define ECHOMAX 255
+#define DIM_MSG 255
 #define PORT 18000
 
 void errorHandler(char *errorMessage) {
     printf(errorMessage);
 }
 
-void ClearWinSock() {
+void clearWinSock() {
 #if defined WIN32
     WSACleanup();
 #endif
+}
+
+void closeConnection(int socket) {
+    closesocket(socket);
+    clearWinSock();
+}
+
+int isVowel(char c) {
+    switch(c) {
+        case 'a': case 'A':
+        case 'e': case 'E':
+        case 'i': case 'I':
+        case 'o': case 'O':
+        case 'u': case 'U':
+            return 1;
+        default:
+            return 0;
+    }
 }
 
 
@@ -35,54 +51,99 @@ int main() {
     WSADATA wsaData;
     int iResult = WSAStartup(MAKEWORD(2 ,2), &wsaData);
     if (iResult != 0) {
-        printf ("error at WSASturtup\n"); return 0;
+        printf ("Error at WSASturtup!\n");
+        return 0;
     }
 #endif
 
     int sock;
-    struct sockaddr_in echoServAddr;
+    struct sockaddr_in serverAddr;
     struct sockaddr_in fromAddr;
+    unsigned int fromAddrLen;
+    char host_name[DIM_MSG];
+    unsigned int port;
+    int msg_len;  //dimensione del messaggio
+    char buffer[DIM_MSG];  //buffer per la ricezione
+    int recvMsgSize;  //dimensione del messaggio ricevuto
+
     unsigned int fromSize;
-    char echoString[ECHOMAX];
-    char echoBuffer[ECHOMAX];
-    char echoBuffer[ECHOMAX];
-    int echoStringLen;
-    int respStringLen;
-
-    printf("Inserisci la stringa echo da inviare al server\n");
-    scanf("%s", echoString);
-    if ((echoStringLen = strlen(echoString)) > ECHOMAX)
-        errorHandler("echo word too long");
-
 
     // CREAZIONE DELLA SOCKET
-    if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
-        errorHandler("socket() failed");
+    if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+        errorHandler("socket() failed!");
+        closeConnection(sock);
+        return 0;
+    }
+
+    //printf("Insert the name of the host you want to contact: ");
+    //scanf("%s", host_name);
+   // printf("Insert the number of port: ");
+    //scanf("%d", &port);
 
     // COSTRUZIONE DELL'INDIRIZZO DEL SERVER
-    memset(&echoServAddr, 0, sizeof(echoServAddr));
-    echoServAddr.sin_family = PF_INET;
-    echoServAddr.sin_port = htons(PORT);
-    echoServAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = PF_INET;
+    serverAddr.sin_port = htons(18000);
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    // INVIO DELLA STRINGA ECHO AL SERVER
-    if (sendto(sock, echoString, echoStringLen, 0, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) !=
-        echoStringLen)
-        errorHandler("sendto() sent different number of bytes than expected");
+    //INVIA PRIMO MESSAGGIO AL SERVER
+    char *msg = "First message send to server";
+    msg_len = strlen(msg);
+    if (sendto(sock, msg, msg_len, 0, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) != msg_len) {
+        errorHandler("sendto() sent different number of bytes than expected!");
+        closeConnection(sock);
+        return 0;
+    }
 
-    // RITORNO DELLA STRINGA ECHO
-    fromSize = sizeof(fromAddr);
-    respStringLen = recvfrom(sock, echoBuffer, ECHOMAX, 0, (struct sockaddr *) &fromAddr, &fromSize);
-    if (echoServAddr.sin_addr.s_addr != fromAddr.sin_addr.s_addr) {
+    //RICEVE OK DAL SERVER
+    fromAddrLen = sizeof(fromAddr);
+    recvMsgSize = recvfrom(sock, buffer, DIM_MSG, 0, (struct sockaddr *) &fromAddr, &fromAddrLen);
+    if (serverAddr.sin_addr.s_addr != fromAddr.sin_addr.s_addr) {
         fprintf(stderr, "Error: received a packet from unknown source.\n");
         exit(1);
     }
+    buffer[recvMsgSize] = '\0';
+    printf("%s\n", buffer);
 
-    echoBuffer[respStringLen] = '\0';
-    printf("Received: %s\n", echoBuffer);
+    //PRENDE STRINGA IN INPUT
+    char str1[DIM_MSG];
+    printf("Insert a string: ");
+    fgets(str1, DIM_MSG, stdin);
 
-    closesocket(sock);
-    ClearWinSock();
+    printf("Uppercase vowel received from server: ");
+    int i = 0;
+    while (str1[i] != '\0') {
+        char c = str1[i];
+        if(isVowel(c) == 1 || c == '\n') {
+            if(isVowel(c) == 1) {
+                if (sendto(sock, &str1[i], 1, 0, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) != 1) {
+                    errorHandler("sendto() sent different number of bytes than expected!");
+                    closeConnection(sock);
+                    return 0;
+                }
+
+
+                fromAddrLen = sizeof(fromAddr);
+                if ((recvMsgSize = recvfrom(sock, buffer, 1, 0, (struct sockaddr *) &fromAddr, &fromAddrLen)) <= 0) {
+                    errorHandler("recvfrom() receive different number of bytes than expected!");
+                    closeConnection(sock);
+                    return 0;
+                }
+                buffer[recvMsgSize] = '\0';
+                printf("%s, ", buffer);
+            }
+            else {
+                if (sendto(sock, &str1[i], 1, 0, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) != 1) {
+                    errorHandler("sendto() sent different number of bytes than expected!");
+                    closeConnection(sock);
+                    return 0;
+                }
+            }
+        }
+        i++;
+    }
+
+    closeConnection(sock);
     system("pause");
     return 0;
 }
